fbd317baa316d892b44fa8f08a87580620172b4c
Rohit Priyadarshi <rohit@Rohits-MacBook-Pro.local>
2012-11-28 00:08:15 -0800

    Implemented the algorithm

--- UniqueStringChar/UniqueStringChar/main.cpp
+++ UniqueStringChar/UniqueStringChar/main.cpp
@@ -9,20 +9,42 @@
 #include <iostream>
 #include <string>
 
-// String generator
-std::string create_string()
+bool has_unique_chars(const std::string& s)
 {
-    std::string s;
-    // get a randon length
-    // get a random character
-    return s;
+    const int MAX_FOLDS = 4;
+    const int MAX_BITS = 32;
+    int used[MAX_FOLDS];
+    
+    memset(used, 0, sizeof(used));
+    for (int i = 0; i < s.size(); ++i) {
+        int q = s[i] / MAX_BITS;
+        int r = s[i] % MAX_BITS;
+        int m = 1 << r;
+        
+        if (used[q] & m) {
+            return false;
+        }
+        used[q] |= m;
+    }
+
+    return true;
+}
+
+void check_unique_chars(const std::string& s)
+{
+    if (has_unique_chars(s)) {
+        std::cout << s << " has unique chars" << std::endl;
+    } else {
+        std::cout << s << " has repeated chars" << std::endl;
+    }
 }
 
 int main (int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::string s1("abcdefghijklmnopqrst");
+    std::string s2("abcdefghijklmnopqrsta");
+    check_unique_chars(s1);
+    check_unique_chars(s2);
     return 0;
 }
 
