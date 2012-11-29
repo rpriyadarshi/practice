c05a0bf1d1d09f64f18e99c4ff39ffd13c730c3e
Rohit Priyadarshi <rohit@Rohits-MacBook-Pro.local>
2012-11-28 20:34:28 -0800

    Initial commit

---
+++ ReverseString/ReverseString/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ReverseString
+//
+//  Created by Rohit Priyadarshi on 11/28/12.
+//  Copyright (c) 2012 Rohit Priyadarshi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
