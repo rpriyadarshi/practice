2ea8c8ef8952aa8bafc1530b20c50f24336fcacd
Rohit Priyadarshi <rohit@rishkan.com>
2020-12-20 23:49:34 -0800

    Achieved correct results in this testcase

--- LFUCache/main.cpp
+++ LFUCache/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <unordered_map>
-
+#include <cassert>
 class LFUCache {
 private: // Classes
     struct Bucket;
+    class PQueue;
 
     struct Blob {
     public: // Data
@@ -11,6 +12,10 @@ private: // Classes
         int _value{0};
     public: // Constructors/destructors
         Blob(int key, int value) : _key(key), _value(value) {}
+    public: // Visualization
+        void print() {
+            std::cout << "[" << _key << ", " << _value << "]" ;
+        }
     };
 
     struct Node {
@@ -24,17 +29,28 @@ private: // Classes
         Node(int key, int value) : _blob(key, value) {}
     public: // Helpers
         void remove() {
-            if (_bucket->_front == this) {
-
-            }
-            if (_bucket->_back == this) {
-
-            }
+            assert(_bucket);
+            _bucket->remove(this);
+            _bucket = nullptr;
         }
     public: // Utility
-        void update(int value) {
+        void upgrade(int value) {
+            _blob._value = value;
+            promote();
         }
-        void touch() {
+        void promote() {
+            assert(_bucket);
+            assert(_bucket->_frequency > 0);
+            // next higher bucket
+            Bucket* ptr = _bucket->insert_front(_bucket->_frequency + 1);
+            remove();
+            ptr->push_front(this);
+        }
+    public: // Visualization
+        void print(const std::string& tab) {
+            std::cout << tab << "[node=" << this << "] [bucket=" << _bucket << ", freq=" << _bucket->_frequency << "] ";
+            _blob.print();
+            std::cout << std::endl;
         }
     };
 
@@ -44,9 +60,45 @@ private: // Classes
         // Controlled by pqueue
         Bucket* _prev{nullptr}; // front
         Bucket* _next{nullptr}; // back
+        PQueue* _queue{nullptr};
         // Controlled here
         Node* _front{nullptr};
         Node* _back{nullptr};
+    public: // Constructors/Destructors
+        explicit Bucket(PQueue* q, int freq) : _queue(q), _frequency(freq) {}
+
+    public: // Helpers
+        Bucket* insert_front(int freq) {
+            assert(_queue);
+            return _queue->insert_front(this, freq);
+        }
+        void remove(Node* ptr) {
+            assert(_front);
+            assert(_back);
+            if (_front == _back) {
+                _front = _back = nullptr;
+            } else {
+                if (_front == ptr) {
+                    Node* next = _front->_next;
+                    next->_prev = nullptr;
+                    _front->_next = nullptr;
+                    _front = next;
+                } else if (_back == ptr) {
+                    Node* prev = _back->_prev;
+                    prev->_next = nullptr;
+                    _back->_prev = nullptr;
+                    _back = prev;
+                } else {
+                    ptr->_prev->_next = ptr->_next;
+                    ptr->_next->_prev = ptr->_prev;
+                    ptr->_prev = nullptr;
+                    ptr->_next = nullptr;
+                }
+            }
+            if (empty()) {
+                _queue->remove(this);
+            }
+        }
     public: // Utility
         bool empty() const {
             return (_front == nullptr);
@@ -59,6 +111,9 @@ private: // Classes
                 ptr->_next = _front;
                 _front = ptr;
             }
+            if (ptr) {
+                ptr->_bucket = this;
+            }
         }
         void push_back(Node* ptr) {
             if (_back == nullptr) {
@@ -68,12 +123,15 @@ private: // Classes
                 ptr->_prev = _back;
                 _back = ptr;
             }
+            if (ptr) {
+                ptr->_bucket = this;
+            }
         }
         Node* remove_front() {
-            Node*ptr = nullptr;
+            Node* ptr = nullptr;
             if (_front != nullptr) {
+                ptr = _front;
                 if (_front == _back) {
-                    ptr = _front;
                     _front = _back = nullptr;
                 } else {
                     Node* next = _front->_next;
@@ -81,14 +139,15 @@ private: // Classes
                     _front->_next = nullptr;
                     _front = next;
                 }
+                ptr->_bucket = nullptr;
             }
             return ptr;
         }
         Node* remove_back() {
-            Node*ptr = nullptr;
+            Node* ptr = _back;
             if (_back != nullptr) {
+                ptr = _back;
                 if (_front == _back) {
-                    ptr = _back;
                     _front = _back = nullptr;
                 } else {
                     Node* prev = _back->_prev;
@@ -96,8 +155,26 @@ private: // Classes
                     _back->_prev = nullptr;
                     _back = prev;
                 }
+                ptr->_bucket = nullptr;
             }
             return ptr;
+        }
+    public: // Visualization
+        void print(const std::string& tab) {
+            std::cout << tab << "[bucket=" << this << "] " << _frequency << std::endl;
+            if (_front == nullptr) {
+                assert(_back == nullptr);
+                std::cout << tab << "[null]" << std::endl;
+            } else {
+                std::string tab2(tab + "    ");
+                std::cout << tab << "nodes [" << std::endl;
+                Node* next = _front;
+                while (next) {
+                    next->print(tab2);
+                    next = next->_next;
+                }
+                std::cout << tab << "]" << std::endl;
+            }
         }
     };
 
@@ -110,11 +187,11 @@ private: // Classes
         Bucket* findOrCreateBucket() {
             Bucket* ptr = nullptr;
             if (_back == nullptr) {
-                ptr = new Bucket();
+                ptr = new Bucket(this, 1);
                 _front = _back = ptr;
             } else {
                 if (_back->_frequency > 1) {
-                    ptr = new Bucket();
+                    ptr = new Bucket(this, 1);
                     _back->_next = ptr;
                     ptr->_prev = _back;
                     _back = ptr;
@@ -125,17 +202,74 @@ private: // Classes
             assert(ptr);
             return ptr;
         }
+        Bucket* insert_front(Bucket* ref, int freq) {
+            assert(ref);
+            assert(_front);
+            assert(_back);
 
+            if (ref->_frequency == freq) {
+                return ref;
+            } else if (ref->_prev && ref->_prev->_frequency == freq) {
+                return ref->_prev;
+            }
+            Bucket* ptr = nullptr;
+            if (ref->_prev == nullptr ||
+                ref->_prev->_frequency > freq) {
+                // Add a bucket with freq
+                ptr = new Bucket(this, freq);
+                if (_front == ref) {
+                    ptr->_next = ref;
+                    _front = ptr;
+                    ref->_prev = ptr;
+                } else {
+                    ptr->_next = ref;
+                    ptr->_prev = ref->_prev;
+                    ref->_prev->_next = ptr;
+                    ref->_prev = ptr;
+                }
+            }
+            return ptr;
+        }
+        void remove(Bucket* ptr) {
+            assert(_front);
+            assert(_back);
+            if (_front == _back) {
+                _front = _back = nullptr;
+            } else {
+                if (_front == ptr) {
+                    Bucket* next = _front->_next;
+                    next->_prev = nullptr;
+                    _front->_next = nullptr;
+                    _front = next;
+                } else if (_back == ptr) {
+                    Bucket* prev = _back->_prev;
+                    prev->_next = nullptr;
+                    _back->_prev = nullptr;
+                    _back = prev;
+                } else {
+                    ptr->_prev->_next = ptr->_next;
+                    ptr->_next->_prev = ptr->_prev;
+                    ptr->_prev = nullptr;
+                    ptr->_next = nullptr;
+                }
+            }
+            assert(ptr->_front == nullptr);
+            assert(ptr->_back == nullptr);
+            delete ptr;
+        }
     public: // Utility
         void push(Node* n) {
             Bucket* ptr = findOrCreateBucket();
             ptr->push_front(n);
         }
-        void pop() {
+        Node* pop() {
+            Node* ptr = nullptr;
             if (_back != nullptr) {
-                _back->remove_back();
+                ptr = _back->remove_back();
                 if (_back->empty()) {
                     if (_back == _front) {
+                        assert(_back->_front == nullptr);
+                        assert(_back->_back == nullptr);
                         delete _back;
                         _front = _back = nullptr;
                     } else {
@@ -144,16 +278,47 @@ private: // Classes
                     }
                 }
             }
+            return ptr;
+        }
+    public: // Visualization
+        void print() {
+            std::cout << "pqueue [" << std::endl;
+            std::string tab("    ");
+            if (_front == nullptr) {
+                assert(_back == nullptr);
+                std::cout << tab << "[null]" << std::endl;
+            } else {
+                std::string tab2(tab + "    ");
+                std::cout << tab << "buckets [" << std::endl;
+                Bucket* next = _front;
+                while (next) {
+                    next->print(tab2);
+                    next = next->_next;
+                }
+                std::cout << tab << "]" << std::endl;
+            }
+            std::cout << "]" << std::endl;
         }
     };
 
 private: // Aliases
-    using Repo = std::unordered_map<int, Node>;
+    using Repo = std::unordered_map<int, Node*>;
 
 private: // Data
     int _capacity;
     Repo _repo;
     PQueue _pqueue;
+
+public: // Visualizations
+    void print(const std::string& msg) {
+        std::cout << "[" << msg << "] --" << std::endl;
+        _pqueue.print();
+        std::cout << "repo [" << std::endl;
+        for (auto& p : _repo) {
+            p.second->print("    ");
+        }
+        std::cout << "]" << std::endl;
+    }
 
 public:
     LFUCache(int capacity) : _capacity(capacity) {
@@ -163,8 +328,9 @@ public:
     int get(int key) {
         auto iter = _repo.find(key);
         if (iter != _repo.end()) {
-            iter->second.touch();
-            return iter->second._blob._value;
+//            iter->second->print("--promote-key(" + std::to_string(key) + ")--");
+            iter->second->promote();
+            return iter->second->_blob._value;
         }
 
         return -1;
@@ -173,11 +339,23 @@ public:
     void put(int key, int value) {
         auto iter = _repo.find(key);
         if (iter == _repo.end()) {
-            Node n(key, value);
-            _repo.emplace(n._blob._key, std::move(n));
-            _pqueue.push(&n);
+            if (_capacity <= _repo.size()) {
+                Node* ptr = _pqueue.pop();
+                assert(ptr);
+                if (ptr != nullptr) {
+                    int erased = _repo.erase(ptr->_blob._key);
+                    assert(erased > 0);
+                    assert(ptr->_prev == nullptr);
+                    assert(ptr->_next == nullptr);
+                    assert(ptr->_bucket == nullptr);
+                }
+                delete ptr;
+            }
+            Node* ptr = new Node(key, value);
+            _repo.emplace(ptr->_blob._key, ptr);
+            _pqueue.push(ptr); // Evict as necessary
         } else {
-            iter->second.update(value);
+            iter->second->upgrade(value);
         }
     }
 };
@@ -188,8 +366,42 @@ public:
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
+// Input
+//  ["LFUCache", "put", "put", "get", "put", "get", "get", "put", "get", "get", "get"]
+//  [[2], [1, 1], [2, 2], [1], [3, 3], [2], [3], [4, 4], [1], [3], [4]]
+// Output
+//  [null, null, null, 1, null, -1, 3, null, -1, 3, 4]
+
+void runtest1() {
+    LFUCache lfu(2);
+    lfu.print("lfu(2)");
+    lfu.put(1, 1);
+    lfu.print("put(1, 1)");
+    lfu.put(2, 2);
+    lfu.print("put(2, 2)");
+    int res0 = lfu.get(1);      // return 1
+    lfu.print("get(1)=" + std::to_string(res0));
+    lfu.put(3, 3);   // evicts key 2
+    lfu.print("put(3, 3)");
+    int res1 = lfu.get(2);      // return -1 (not found)
+    lfu.print("get(2)=" + std::to_string(res1));
+    int res2 = lfu.get(3);      // return 3
+    lfu.print("get(3)=" + std::to_string(res2));
+    lfu.put(4, 4);   // evicts key 1.
+    lfu.print("put(4, 4)");
+    int res3 = lfu.get(1);      // return -1 (not found)
+    lfu.print("get(1)=" + std::to_string(res3));
+    int res4 = lfu.get(3);      // return 3
+    lfu.print("get(3)=" + std::to_string(res4));
+    int res5 = lfu.get(4);      // return 4
+    lfu.print("get(4)=" + std::to_string(res5));
+
+    std::cout << "[null, null, null, 1, null, -1, 3, null, -1, 3, 4]" << std::endl;
+    std::cout << "[null, null, null, " << res0 << ", null, " << res1 << ", " << res2 << ", null, " << res3 << ", " << res4 << ", " << res5;
+    std::cout << "]" << std::endl;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    runtest1();
     return 0;
 }
