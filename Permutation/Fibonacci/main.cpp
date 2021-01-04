3fbefff715ba44f93446b8ffa62acf11ac2f24a0
Rohit Priyadarshi <rohit@rishkan.com>
2021-01-03 21:55:11 -0800

    Added O(N) memoization.

--- Permutation/Fibonacci/main.cpp
+++ Permutation/Fibonacci/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int64_t fibonacci_slow(int64_t n) {
     if (n <= 0) {
@@ -8,6 +9,19 @@ int64_t fibonacci_slow(int64_t n) {
     } else {
         return fibonacci_slow(n - 1) + fibonacci_slow(n - 2);
     }
+}
+
+int64_t fibonacci_memo(int64_t n, std::vector<int64_t>& memo) {
+    if (n <= 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else if (memo[n] > 0) {
+        return memo[n];
+    }
+
+    memo[n] = fibonacci_memo(n - 1, memo) + fibonacci_memo(n - 2, memo);
+    return memo[n];
 }
 
 int64_t fibonacci_fast(int64_t n) {
@@ -27,6 +41,13 @@ int main() {
         std::cout << fibonacci_fast(i) << " ";
     }
     std::cout << std::endl;
+
+    std::vector<int64_t> memo(n + 1);
+    for (int64_t i = 0; i < n; i++) {
+        std::cout << fibonacci_memo(i, memo) << " ";
+    }
+    std::cout << std::endl;
+
     for (int64_t i = 0; i < n; i++) {
         std::cout << fibonacci_slow(i) << " ";
     }
