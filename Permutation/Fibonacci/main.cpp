f0d307f82c5e9a4269b327f273cd6ad512bdfef9
Rohit Priyadarshi <rohit@rishkan.com>
2021-01-03 21:24:24 -0800

    Difference between O(1.6^N) or O(2^N) vs O(N) implementation.

---
+++ Permutation/Fibonacci/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int64_t fibonacci_slow(int64_t n) {
+    if (n <= 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fibonacci_slow(n - 1) + fibonacci_slow(n - 2);
+    }
+}
+
+int64_t fibonacci_fast(int64_t n) {
+    int64_t v1 = 0;
+    int64_t v2 = n <= 0 ? 0 : 1;
+    for (int64_t i = 1; i < n; i++) {
+        int64_t sum = v1 + v2;
+        v1 = v2;
+        v2 = sum;
+    }
+    return v2;
+}
+
+int main() {
+    int64_t n = 50;
+    for (int64_t i = 0; i < n; i++) {
+        std::cout << fibonacci_fast(i) << " ";
+    }
+    std::cout << std::endl;
+    for (int64_t i = 0; i < n; i++) {
+        std::cout << fibonacci_slow(i) << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
